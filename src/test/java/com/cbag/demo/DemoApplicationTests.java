ef7773c6352bb74b860851e023e84045ad180dc8
cbag1 <bacargoudiaby@gmail.com>
2023-11-11 21:34:16 +0000

    NEW JAVA

---
+++ src/test/java/com/cbag/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbag.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
